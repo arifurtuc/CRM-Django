f1243490980c270755a28596a2435f70974c9c0e
arif <arifur.tuc@gmail.com>
2024-03-17 15:56:34 +0100

    Updating home page

--- clientapp/views.py
+++ clientapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
+    """
+    View function for the homepage.
+
+    This function renders the 'index.html' template,
+    which represents the homepage of the CRM application.
+    """
     return render(request, 'clientapp/index.html')
