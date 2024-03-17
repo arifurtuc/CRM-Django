cacae97d8d5e9efc67853fd90d718649ebace8a7
arif <arifur.tuc@gmail.com>
2024-03-17 21:40:38 +0100

    Implementing user registration and login

--- clientapp/urls.py
+++ clientapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('user-login', views.user_login, name='user-login'),
 ]
--- clientapp/views.py
+++ clientapp/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
+# Homepage
 def home(request):
     """
     View function for the homepage.
@@ -8,3 +12,59 @@ def home(request):
     which represents the homepage of the CRM application.
     """
     return render(request, 'clientapp/index.html')
+
+# User Registration
+def register(request):
+    """
+    View function for user registration.
+
+    Renders the registration form for new users.
+    If the form is submitted with valid data, it saves the user and redirects to the login page.
+    """
+    form = RegistrationForm()
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('user-login')
+    
+    context = {'form':form}
+    return render(request, 'clientapp/register.html', context=context)
+
+# User Login
+def user_login(request):
+    """
+    View function for user login.
+
+    Renders the login form for existing users.
+    If the form is submitted with valid credentials, it authenticates the user and logs them in.
+    """
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                #return redirect('')
+
+    context = {'form':form}
+    return render(request, 'clientapp/user-login.html', context=context)
+
+# User Logout
+def user_logout(request):
+    """
+    View function for user logout.
+
+    Logs out the currently logged-in user and redirects to the login page.
+    """
+    auth.logout(request)
+    return redirect('user-login')
