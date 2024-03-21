e353e9fb97c2e24ba52af0d73749327d48a7adb3
arif <arifur.tuc@gmail.com>
2024-03-21 22:53:36 +0100

    Adding Cleint model

--- clientapp/models.py
+++ clientapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Define a model for storing client information
+class Client(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=300)
+    phone = models.CharField(max_length=20)
+    street_name = models.CharField(max_length=300)
+    city = models.CharField(max_length=255)
+    province = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
